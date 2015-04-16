c170708b2b290d583b89d137f2fe4b384b7703db
Helmut Januschka <helmut@januschka.com>
2015-04-16 10:45:41 +0200

    Update buffer.h

--- buffer.h
+++ buffer.h
@@ -5,8 +5,8 @@
 // Copyright (c) 2012 TJ Holowaychuk <tj@vision-media.ca>
 //
 
-#ifndef BUFFER_H
-#define BUFFER_H 1
+#ifndef CLIB_BUFFER_H
+#define CLIB_BUFFER_H 1
 
 #include <sys/types.h>
 
@@ -14,8 +14,8 @@
  * Default buffer size.
  */
 
-#ifndef BUFFER_DEFAULT_SIZE
-#define BUFFER_DEFAULT_SIZE 64
+#ifndef CLIB_BUFFER_DEFAULT_SIZE
+#define CLIB_BUFFER_DEFAULT_SIZE 64
 #endif
 
 /*
@@ -26,76 +26,76 @@ typedef struct {
   size_t len;
   char *alloc;
   char *data;
-} buffer_t;
+} clib_buffer_t;
 
 // prototypes
 
-buffer_t *
-buffer_new();
+clib_buffer_t *
+clib_buffer_new();
 
-buffer_t *
-buffer_new_with_size(size_t n);
+clib_buffer_t *
+clib_buffer_new_with_size(size_t n);
 
-buffer_t *
-buffer_new_with_string(char *str);
+clib_buffer_t *
+clib_buffer_new_with_string(char *str);
 
-buffer_t *
-buffer_new_with_string_length(char *str, size_t len);
+clib_buffer_t *
+clib_buffer_new_with_string_length(char *str, size_t len);
 
-buffer_t *
-buffer_new_with_copy(char *str);
-
-size_t
-buffer_size(buffer_t *self);
+clib_buffer_t *
+clib_buffer_new_with_copy(char *str);
 
 size_t
-buffer_length(buffer_t *self);
+clib_buffer_size(clib_buffer_t *self);
+
+size_t
+clib_buffer_length(clib_buffer_t *self);
 
 void
-buffer_free(buffer_t *self);
+clib_buffer_free(clib_buffer_t *self);
 
 int
-buffer_prepend(buffer_t *self, char *str);
+clib_buffer_prepend(clib_buffer_t *self, char *str);
 
 int
-buffer_append(buffer_t *self, const char *str);
+clib_buffer_append(clib_buffer_t *self, const char *str);
 
 int
-buffer_appendf(buffer_t *self, const char *format, ...);
+clib_buffer_appendf(clib_buffer_t *self, const char *format, ...);
 
 int
-buffer_append_n(buffer_t *self, const char *str, size_t len);
+clib_buffer_append_n(clib_buffer_t *self, const char *str, size_t len);
 
 int
-buffer_equals(buffer_t *self, buffer_t *other);
+clib_buffer_equals(clib_buffer_t *self, clib_buffer_t *other);
 
 ssize_t
-buffer_indexof(buffer_t *self, char *str);
+clib_buffer_indexof(clib_buffer_t *self, char *str);
 
-buffer_t *
-buffer_slice(buffer_t *self, size_t from, ssize_t to);
+clib_buffer_t *
+clib_buffer_slice(clib_buffer_t *self, size_t from, ssize_t to);
 
 ssize_t
-buffer_compact(buffer_t *self);
+clib_buffer_compact(clib_buffer_t *self);
 
 void
-buffer_fill(buffer_t *self, int c);
+clib_buffer_fill(clib_buffer_t *self, int c);
 
 void
-buffer_clear(buffer_t *self);
+clib_buffer_clear(clib_buffer_t *self);
 
 void
-buffer_trim_left(buffer_t *self);
+clib_buffer_trim_left(clib_buffer_t *self);
 
 void
-buffer_trim_right(buffer_t *self);
+clib_buffer_trim_right(clib_buffer_t *self);
 
 void
-buffer_trim(buffer_t *self);
+clib_buffer_trim(clib_buffer_t *self);
 
 void
-buffer_print(buffer_t *self);
+clib_buffer_print(clib_buffer_t *self);
 
-#define buffer_string(self) (self->data)
+#define clib_buffer_string(self) (self->data)
 
 #endif
